8181d0056579297feaababc46ad11c325fc12fbc
samuel-poinama <samuel.poinama@efrei.net>
2024-02-14 18:24:05 +0100

    create routes

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <p>Welcome to the login page</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'loginView'
+}
+</script>
---
+++ src/views/MailView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Mail</h1>
+        <p>Welcome to the mail page</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'mailView'
+}
+</script>
