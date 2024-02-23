aa5e017453d0e818e47692f42e8a0073417d7f06
Dragan Constantin <constantin.dragan@efrei.net>
2024-02-23 23:40:24 +0100

    Update MailView.vue

--- src/views/MailView.vue
+++ src/views/MailView.vue
@@ -3,10 +3,16 @@
         <h1>Mail</h1>
         <p>Welcome to the mail page</p>
     </div>
+    <NewMailWidget/>
 </template>
 
 <script>
+import NewMailWidget from '@/components/NewMailWidget.vue'
+
 export default {
-    name: 'mailView'
+    name: 'mailView',
+    components: {
+        NewMailWidget
+    }
 }
 </script>
