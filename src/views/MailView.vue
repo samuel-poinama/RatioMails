8f33376c6b9feea5fbc6b9943113c715375c552d
TheSpaceMan88 <alex.poissonneau@yahoo.fr>
2024-02-23 18:06:39 +0100

    updates 	modified:   src/views/MailView.vue

--- src/views/MailView.vue
+++ src/views/MailView.vue
@@ -3,6 +3,22 @@
         <h1>Mail</h1>
         <p>Welcome to the mail page</p>
     </div>
+    <div>
+        <div>
+            <h1>Your Mail</h1>
+                <div>
+
+                    <!--  <li v-for="item in items">        --> 
+                    <!--  <{{ item.message }}       --> 
+                    
+                    
+                </div>
+        </div>
+        <div>
+            <h1>Viewver</h1>
+            <p></p>
+        </div>
+    </div>
 </template>
 
 <script>
