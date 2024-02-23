2d42ae13f38b6caba5b8f31a1028dbd65086f3ec
TheSpaceMan88 <alex.poissonneau@yahoo.fr>
2024-02-23 18:40:37 +0100

    updates 	modified:   src/views/MailView.vue

--- src/views/MailView.vue
+++ src/views/MailView.vue
@@ -3,18 +3,23 @@
         <h1>Mail</h1>
         <p>Welcome to the mail page</p>
     </div>
-    <div>
-        <div>
+    <div class="flex w-full justify-evenly">
+        <div class="w-5/8">
             <h1>Your Mail</h1>
                 <div>
-
+                    <li >
                     <!--  <li v-for="item in items">        --> 
-                    <!--  <{{ item.message }}       --> 
-                    
+                        <div class="border-2 rounded-lg bg-slate-800 text-gray-100" @click="1+1">
+                            <!--  <{{ item.from }}       --> 
+                            <h3 class="font-bold">Gérard Depardieu</h3>
+                            <!--  <{{ item.object }}       --> 
+                            <p>Films </p>
+                        </div>
+                    </li> 
                     
                 </div>
         </div>
-        <div>
+        <div class="w-3/8">
             <h1>Viewver</h1>
             <p></p>
         </div>
